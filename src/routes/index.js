2929802202126e829ad2dfaa2481243921ba716a
Agustin Morro <agustin.morro@gmail.com>
2023-09-12 19:41:01 -0300

    first-commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const actorRouter = require('./actor.router');
+const directorRouter = require('./director.router');
+const genreRouter = require('./genre.router');
+const movieRouter = require('./movie.router');
 
 // colocar las rutas aquí
-
+router.use('/movies', movieRouter);
+router.use('/actors', actorRouter);
+router.use('/directors',directorRouter);
+router.use('/genres', genreRouter);
 
 module.exports = router;
